485b7c10fed9cc2143687e62aee44504d7be92b0
Leonard Sangoroh <leonardsangoroh@gmail.com>
2023-01-24 16:32:30 +0300

    First commit

---
+++ dayOne.swift
@@ -0,0 +1,18 @@
+// Variables
+
+// creating a variable using 'var' makes it changeable
+var favoriteShow = "Family guy"
+
+favoriteShow = "Rick and Morty"
+
+// Swift is type safe - data types are determined by swift
+
+// Multi line strings
+
+var lee = """ 
+Name is Lee Leonard 
+Currently Learning swift
+Finally taking a single roadmap this year
+"""
+
+print(lee)
