6902680358271de5046f944d551f8c047938325d
leonardsangoroh <leonardsangoroh@gmail.com>
2023-02-02 16:55:47 +0300

    day five complete

--- dayFive.swift
+++ dayFive.swift
@@ -92,3 +92,105 @@ func greet ( _ person: String) {
 }
 
 greet("Lee")
+
+//Default parameters
+/*
+print() function has a default parameter that creates a linebreak
+when the print() function is called.
+We can also set defualt values to our parameters
+*/
+
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello \(person)")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+
+/* 
+Because we already set a default, on the 'nicely' parameter,
+we can call the function in 2 ways
+*/
+greet("Lee")
+// or
+greet("leonard", nicely: false)
+
+/*
+An example where default parameters can be used is when 
+writing a function to find the shortest rout btw 2 points
+Parameters are; to, from, route, avaidHighways
+One can set route to 'fastest' and avaidHighways to false 
+*/
+
+//Variadic functions
+/*
+These are functions that recieve any numbers
+number of input of the same data type
+An example is the print() function
+*/
+print("Lee", "Leonard", "Sangoroh")
+
+/*
+Parameters are made variadic by writing ...
+after its type
+*/
+
+func squareVariadic(numbers: Int...) {
+    for number in numbers {
+        print("\(number) squared is \(number*number)")
+    }
+}
+
+//calling it 
+squareVariadic(numbers: 1, 2, 3, 4, 5)
+
+//Variadic functions can be used when the input length is indefinite
+
+//Writing throwing functions
+
+//Helps in throwing errors
+
+/*
+We first need to define an enum that describes the errors
+we can throw. The enum is based on Swift existing Error type
+*/
+enum PasswordError: Error {
+    case obvious
+}
+
+/*
+checkPassword() function that throws error if something 
+goes wrong
+*/
+func checkPassword(_ Password: String) throws -> Bool {
+    if Password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+//Running throwing functions
+
+do {
+    try checkPassword("password")
+    print("The password is good")
+} catch {
+    print("You can't use that password")
+}
+
+// inout parameters
+/*
+All parameters passed into a Swift function are constants
+However, you can pass 1 or more parameters as inout, meaning
+they can be changed inside the function and those changes can
+reflect in the original value outside the function
+*/
+
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+
+var myNum = 10
+doubleInPlace(number: &myNum)
